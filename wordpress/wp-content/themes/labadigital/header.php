a111a1bc7585d8bc7a27b19573e262bfea9e3b63
Francesca Barbagallo <you@example.com>
2017-06-20 16:01:38 +0200

    installed wp - theme  labadigital - css to scss

---
+++ wordpress/wp-content/themes/labadigital/header.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package understrap
+ */
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<div class="hfeed site" id="page">
+
+	<!-- ******************* The Navbar Area ******************* -->
+	<div class="row">
+ 		<div class="col1 clearfix">
+ 			 <div class="colonna-1-3"><img class="logotype" src="../img/logo_blue.svg" alt="logotype labadigital"></div>
+ 			 <div class="colonna-1-5">
+ 					<nav role="custom-dropdown">
+ 						 <input type="checkbox" id="button">
+ 						 <label for="button" onclick></label>
+ 						 <ul>
+ 								<li><a href="#">Home</a></li>
+ 								<li><a href="#" class="attivo">Corsi</a></li>
+ 								<li><a href="#">Web Design</a></li>
+ 								<li><a href="#">App Design</a></li>
+ 								<li><a href="#">Contatti</a></li>
+ 								<li><a href="#">Laba</a></li>
+ 						 </ul>
+ 					</nav>
+ 			 </div>
+ 			 <!--chiudo nav 1-7-->
+ 		</div>
+ 		<!--/* chiudo row */-->
+					<!-- Your site title as branding in the menu -->
+					<?php if ( ! has_custom_logo() ) { ?>
+
+						<?php if ( is_front_page() && is_home() ) : ?>
+
+							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+
+						<?php else : ?>
+
+							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+
+						<?php endif; ?>
+
+
+					<?php } else {
+						the_custom_logo();
+					} ?><!-- end custom logo -->
+
+				<!-- The WordPress Menu goes here -->
+				<?php wp_nav_menu(
+					array(
+						'theme_location'  => 'primary',
+						'container_class' => 'collapse navbar-collapse',
+						'container_id'    => 'navbarNavDropdown',
+						'menu_class'      => 'navbar-nav',
+						'fallback_cb'     => '',
+						'menu_id'         => 'main-menu',
+						'walker'          => new WP_Bootstrap_Navwalker(),
+					)
+				); ?>
+			<?php if ( 'container' == $container ) : ?>
+			</div><!-- .container -->
+			<?php endif; ?>
+
+		</nav><!-- .site-navigation -->
+
+	</div><!-- .wrapper-navbar end -->
